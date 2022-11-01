463a1e4edd8f39a71aef4e7edd45eacb559d2fad
Lidia Velicia <lvelirui@myuax.com>
2022-11-01 20:21:28 +0100

    añadir comentarios en el código de ej05-1 y ej05-2

--- Ej05-1_Bloque-Iterativo-Finito.py
+++ Ej05-1_Bloque-Iterativo-Finito.py
@@ -1,6 +1,3 @@
-#Esta vez, no se trata de determinar si hay que ejecutar o no un bloque, sino de determinar si hay que repetirlo. 
-# Este bloque podrá repetirse de 0 a n veces.
-#
 #EJERCICIO:
 #Imaginemos que pedimos al usuario introducir un número comprendido entre 1 y 10, aunque esta vez, 
 # le pedimos un valor en caso de error en lugar de salir del programa, # tantas veces como se equivoque.
@@ -13,19 +10,28 @@ try:
     numero = int(numero)
 except:
     numero = 0 #si no se puede convertir a entero, se le asigna el valor 0
-    #esto va a hacer que numero no cumpla la condición del bucle, y se siga pidiendo al usuario que introduzca un número entre 1 y 10
+    #esto va a meternos directamente en el bucle iterativo, que nos pedirá introducir de nuevo un número entre el 1 y el 10
 
-#Mientras el número no esté entre 1 y 10
-while not 1 <= numero <= 10:
+# Mientras el número no esté entre 1 y 10
+while not 1 <= numero <= 10: #ENTRAMOS EN EL BUCLE FINITO
     #El número no es válido
     #y le volvemos a pedir al usuario que introduzca un número
     numero = input("Introduzca otra vez un número entre 1 y 10: ")
-
     try:
         numero = int(numero)
     except:
         numero = 0
-    
+
+# Si el número introducido está entre 1 y 10:   
 print("Estamos seguros de que el número introducido ", numero, "está comprendido entre 1 y 10 incluidos")
 
+
+#COMENTARIO:
+#El bucle no tendría por qué llegar a ejecutarse, si a la primera introducimos un número entre 1 y 10.
+#Por eso los bucles finitos pueden se dice que pueden repetirse entre 0 y n veces.
+#
+#FORMA DE USO:
+#Lo que estamos haciendo es crear un bucle finito con las condiciones opuestas a las que queremos verificar.
+#De tal forma que, si no se están dando las condiciones que queremos, se repite el experimento hasta que se cumplen.
+#Una vez se cumplen, se continúa con el código.
 
--- Ej05-2_Bloque-Iterativo-Infinito.py
+++ Ej05-2_Bloque-Iterativo-Infinito.py
@@ -4,20 +4,32 @@
 
 #LO VAMOS A RESOLVER USANDO UN BUCLE ITERATIVO INFINITO
 
-while True: #entramos en un bucle infinito, esto es, se va a repetir siempre, a no ser que se den las condiciones que queramos
+while True: #ENTRAMOS EN UN BUCLE INFINITO
+            #esto es, se va a repetir siempre, a no ser que se den unas condiciones
     #el usuario introduce un número
     numero = input("Introduzca un número entre 1 y 10: ")
     
     try: #intentamos trasnformarlo a entero
         numero = int(numero) #si se puede, se asigna a la variable numero
     except:
-        pass #si no se puede, se repite el bucle
+        pass #si no se puede convertir, se repite el bucle
 
     #una vez tenemos el entero
-    else: #establecemos la condición necesaria para salir del bucle infinito
+    else: #establecemos la CONDICIÓN NECESARIA PARA SALIR DEL BUCLE INFINITO
         if 1 <= numero <= 10:
             #si el número está entre 1 y 10, salimos del bucle
             break #INDICADOR SALIR BUCLE INFINITO
 
-#si hemos conseguido salir del bucle es poruq el numero introducido está entre el 1 y el 10, por lo tanto:
+#si hemos conseguido salir del bucle es porque el numero introducido está entre el 1 y el 10, por lo tanto:
 print("El número",numero,"está comprendido entre el 1 y el 10 incluidos")
+
+
+#COMENTARIO:
+#Lo que estamos haciendo es crear un bucle infinito de pedir al usuario y número entre el 1 y el 10, 
+# del que sólo podremos salir si se cumple la condición de que el número es un entero y está entre el 1 y el 10.
+# Por lo tanto, el resto del código sólo se podrá ejecutar una vez hayamos cumplido dichas condiciones.
+
+#FORMA DE USO: 
+# nuestra forma de asegurarnos de que se cumplen unas condiciones es si el código posterior al bucle se ejecuta.
+# Esto se da, sólo si se han cumplido las condiciones establecidas en el 'else:' (nuestras condiciones).
+
